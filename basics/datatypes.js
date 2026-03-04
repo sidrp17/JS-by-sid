47cf3c8ff7b76f27b963ebad840c4ea12672475f
siddhraj17 <114302194+sidrp17@users.noreply.github.com>
2026-03-04 05:39:38 +0000

    lets find datatypes in JS

---
+++ basics/datatypes.js
@@ -0,0 +1,19 @@
+"use strict"; // treat all js code as newer version
+
+//alert(3+3) // we are using nodejs not browser
+
+/*1 number 
+    bigint
+2 string
+3 Boolean
+4 null => standalone value
+5 undefined
+6 Symbol =>unique
+
+//object*/
+let firstName  = "sandy"
+console.log(typeof firstName);
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
+
