2b1fe54b965d7a6929a341aad6895d1aba6990c7
siddhraj17 <114302194+sidrp17@users.noreply.github.com>
2026-03-04 05:07:58 +0000

    investigate variables in JS

---
+++ basics/variables.js
@@ -0,0 +1,14 @@
+const accountId = 15698
+let accountEmail = "sidpandit@google.com"
+var accountPassword = "12345"
+accountCity = "jaipur"//allowed but not prefered
+let accountState //undefined
+
+//accountId = 2 changes not allowed in const
+console.log(accountId);
+
+//another method for console.log
+console.table([accountEmail,accountEmail,accountPassword,accountCity,accountState])
+
+/* prefer not to use var because of issue  
+ */
