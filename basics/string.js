1b772049dac6c08c3f6bacaf027741e75a0d4914
siddhraj17 <114302194+sidrp17@users.noreply.github.com>
2026-03-05 07:19:51 +0000

    analysis of string in js

---
+++ basics/string.js
@@ -0,0 +1,14 @@
+const name ="siddhraj"
+const repoCount = 71
+
+//console.log(name + repoCount); // not recomm...
+
+console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
+// other methods
+const gameName = new String('siddhraj pandit')
+//console.log(gameName);
+//console.log(gameName [7]);
+//console.log(gameName.length);
+//console.log(gameName.toUpperCase());
+console.log(gameName.charAt(3));
+
