7bb2406c274eb339540f306b7fb9519fe66bc939
siddhraj17 <114302194+sidrp17@users.noreply.github.com>
2026-03-12 06:19:13 +0000

    study of numbers in JS

---
+++ basics/num_and_maths.js
@@ -0,0 +1,18 @@
+const score = 100
+console.log(score);
+
+const balence = new Number(400); //defining new object for num
+console.log(balence);
+
+console.log(balence.toString());
+console.log(balence.toString().length);
+
+console.log(balence.toFixed(2));
+
+const otherNum = 20.179
+console.log(otherNum.toPrecision(3));
+
+const numberNew = 1000000
+console.log(numberNew.toLocaleString('en-IN'));
+
+//================= maths ==========
