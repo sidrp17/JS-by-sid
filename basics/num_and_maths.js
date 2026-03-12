2402705f1c5dd8e2f9dbe3ce91ce018c3bffaa57
siddhraj17 <114302194+sidrp17@users.noreply.github.com>
2026-03-12 07:02:21 +0000

    in detail maths in js

--- basics/num_and_maths.js
+++ basics/num_and_maths.js
@@ -16,7 +16,19 @@ const numberNew = 1000000
 console.log(numberNew.toLocaleString('en-IN')); */
 
 //================= maths ==========
-console.log(Math);
-console.log(Math.abs(-7));
-console.log(Math.round(23.3));
+//console.log(Math);
+//console.log(Math.abs(-7));
+//console.log(Math.round(23.3));
+//console.log(Math.ceil(4.3));
+//console.log(Math.floor(4.3));
 
+console.log(Math.random()); // the value is between 0 and 1
+console.log((Math.random()*10)+1);
+console.log(Math.floor((Math.random()*10)+1));
+
+// defining min and max
+
+const min = 10
+const max = 30
+
+console.log(Math.floor(Math.random()*(max-min +1))+min);
