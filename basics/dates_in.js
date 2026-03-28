3ec461a13b73ab61dae054b7937c5b45bee1b09a
siddhraj17 <114302194+sidrp17@users.noreply.github.com>
2026-03-28 06:44:52 +0000

    how to access dates in js

---
+++ basics/dates_in.js
@@ -0,0 +1,16 @@
+let myDate = new Date()
+/* console.log(myDate);
+console.log(myDate.toString());
+console.log(myDate.toISOString);
+console.log(myDate.toDateString());
+console.log(myDate.toJSON());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toLocaleTimeString()); */
+
+//another methods
+
+let newDate = new Date(1997,  11, 4)// format [yyyy, mm, dd] and month start from zero
+console.log(newDate.toDateString());
+
+let allDateTime = new Date(2001, 2, 27, 7, 0)
+console.log(allDateTime.toLocaleString());
